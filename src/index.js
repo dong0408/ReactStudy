a8540aa5a2cbebcd212ff1b549f6055020cc6c72
xudong <xuyadong@abupdate.com>
2023-04-20 20:29:13 +0800

    feat:纯组件

--- src/index.js
+++ src/index.js
@@ -2,14 +2,49 @@ import React, { Component } from "react";
 // import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 class App extends Component {
+  state = {
+    count: 0,
+    isShow: false,
+  };
+  inputRef = React.createRef();
   render() {
     return (
       <div>
         1q2F
         <button onClick={this.handleClick}>+!</button>
+        <div>{this.state.count}</div>
+        {this.state.isShow ? (
+          <input type="text"    ref={this.inputRef}></input>
+        ) : (
+          <button onClick={this.hadleBtn}>发表评论</button>
+        )}
       </div>
     );
   }
+  handleClick = () => {
+    // this.setState({
+    //   count: this.state.count + 1,
+    // });
+    this.setState((state) => {
+      return {
+        count: state.count + 1,
+      };
+    });
+    this.setState((state) => {
+      return {
+        count: state.count + 1,
+      };
+    });
+  };
+  hadleBtn = () => {
+    this.setState(
+      {
+        isShow: true,
+      },
+      () => {
+        this.inputRef.current.focus();
+      });
+  };
 }
 const container = document.getElementById("root");
 const root = createRoot(container);
